dc251b5c35005be0e1e809b250124ae6dbab3263
Huszar Beata <beata.huszar@adevinta.com>
2019-09-20 15:35:34 +0200

    Rendering list of components

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,8 +1,11 @@
 import React from 'react';
 
-const ImageList = (props) => {
-    console.log(props.images);
-    return <div>ImageList</div>;
+const ImageList = props => {
+    // .map() makes a brand new array, so we can spare a for loop with it
+    const images = props.images.map(({ urls, description, id }) => {
+        return <img src={urls.regular} alt={description} key={id} />;
+    });
+    return <div className="image-list" >{images}</div>;
 };
 
 export default ImageList;
