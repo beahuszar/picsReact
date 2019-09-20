337b71ccbb6afa105c8ef78420fb9470e9bda555
Huszar Beata <beata.huszar@adevinta.com>
2019-09-20 11:45:05 +0200

    Axios get request to API

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,23 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
-    onSearchSubmit(term){
-        console.log(term);
+    onSearchSubmit(term) {
+        // Endpoint  + an object for request specification
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID cacffa66c2846ef57a79d530cc918ee0c604dcb5cd5eea7f4dbea2eca5dbeac1'
+            }
+        });
     };
-    
+
     render() {
         return (
-        <div className="ui container" style={{ marginTop: '10px' }}>
-            <SearchBar formSubmitted={this.onSearchSubmit} />
-        </div>
+            <div className="ui container" style={{ marginTop: '10px' }}>
+                <SearchBar formSubmitted={this.onSearchSubmit} />
+            </div>
         );
     };
 }
