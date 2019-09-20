8025e93dcc6d08b702ffad82891794b56f96428e
Huszar Beata <beata.huszar@adevinta.com>
2019-09-20 14:05:36 +0200

    Async await request

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,16 @@ import axios from 'axios';
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
-    onSearchSubmit(term) {
+    async onSearchSubmit(term) {
         // Endpoint  + an object for request specification
-        axios.get('https://api.unsplash.com/search/photos', {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
             params: { query: term },
             headers: {
                 Authorization: 'Client-ID cacffa66c2846ef57a79d530cc918ee0c604dcb5cd5eea7f4dbea2eca5dbeac1'
             }
         });
+
+        console.log(response.data.results);
     };
 
     render() {
