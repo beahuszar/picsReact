61f20a73ede5c8a72953bbe5ef227f0efba35b8a
Huszar Beata <beata.huszar@adevinta.com>
2019-10-01 11:42:26 +0200

    Callbacks on image load

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,6 +1,24 @@
 import React from 'react';
 
 class ImageCard extends React.Component {
+    constructor(props) {
+        super(props);
+
+        // instance variable for accessing DOM elements
+        // instead of document.queryselector() ect. 
+        this.imageRef = React.createRef();
+    };
+
+    componentDidMount() {
+        // we need an event listener to wait for image load
+        // to be able to retrieve the image height
+        this.imageRef.current.addEventListener('load', this.setSpans);
+    };
+
+    setSpans = () => {
+        console.log(this.imageRef.current.clientHeight);
+    }
+
     render() {
         // desctructuring => desc & urls are the properties of this.props.image
         // after this, we can use the short version
@@ -9,6 +27,7 @@ class ImageCard extends React.Component {
         return (
             <div>
                 <img
+                    ref={this.imageRef}
                     alt={description}
                     src={urls.regular}
                 />
